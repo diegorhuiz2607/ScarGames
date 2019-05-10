8d4f91e2bd7204358bcf061b4a1af849dc22d17c
Diego Rhuiz <diegorhuiz2607@gmail.com>
2019-05-10 10:16:56 -0300

    primeiro Commit

---
+++ ScarGames/src/java/br/com/scargames/controller/BandeiraMB.java
@@ -0,0 +1,96 @@
+package br.com.scargames.controller;
+
+import br.com.scargames.domain.Bandeira;
+import br.com.scargames.services.BandeiraService;
+import br.com.scargames.util.UtilMessages;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean(name = "bandeiraMB")
+@SessionScoped
+public class BandeiraMB implements Serializable{
+
+    private Bandeira bandeira;
+    private List<Bandeira> bandeiras;
+    
+    public BandeiraMB() {
+        this.listar();
+    }
+    
+    public void listar(){
+        BandeiraService service = new BandeiraService();
+        bandeiras = service.listar();
+    }
+    
+    public String novo(){
+        bandeira = new Bandeira();
+        return "new.xhtml?faces-redirect=true";
+    }
+    
+    public String inserir(){
+        BandeiraService service = new BandeiraService();
+        if (service.inserir(bandeira)){
+                UtilMessages.messageInfo("Bandeira cadastrada com sucesso!");
+            this.listar();
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            UtilMessages.messageError("Ocorreu um erro ao cadastrar a bandeira");
+            return null;
+        }
+    }
+    
+    public String alterar(){
+        BandeiraService service = new BandeiraService();
+        if (service.alterar(bandeira)){
+            UtilMessages.messageInfo("Bandeira cadastrada com sucesso!");
+            this.listar();
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            UtilMessages.messageError("Ocorreu um erro ao alterar a bandeira");
+            return null;
+        }
+    }
+    
+    public String carregarDados(Bandeira bandeira){
+        this.bandeira = bandeira;
+        return "alter.xhtml?faces-redirect=true";
+    }
+    
+    public String excluir(Bandeira bandeira){
+        BandeiraService service = new BandeiraService();
+        if (service.excluir(bandeira)){
+            UtilMessages.messageInfo("Bandeira excluida com sucesso!");
+            this.listar();
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            UtilMessages.messageError("Ocorreu um erro ao excluir a bandeira");
+            return null;
+        }
+    }
+    
+    public String cancelar(){
+        return "list.xhtml?faces-redirect=true";
+    }
+
+    public List<Bandeira> getBandeiras() {
+        return bandeiras;
+    }
+
+    public void setBandeiras(List<Bandeira> bandeiras) {
+        this.bandeiras = bandeiras;
+    }
+
+    public Bandeira getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(Bandeira bandeira) {
+        this.bandeira = bandeira;
+    }
+}
